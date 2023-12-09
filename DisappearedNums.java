ca83293222c04e87c41fb907aaade0d132b75d22
SDE47 <sanketdevghare@gmail.com>
2023-12-09 23:23:54 +0530

    some changes made

---
+++ DisappearedNums.java
@@ -0,0 +1,19 @@
+package Sanket;
+
+import java.util.Arrays;
+
+/* Q: Given array an which is supposed to have the all numbers from 1 to N, but it has few numbsers missing.
+find those numsbers.
+For ex. Rang of array is 1 to 5. Expected array = [1,2,3,4,5]
+Given array= [2,4,2]  now here the missing numbers are [1,3,5] -->This is the answer.
+ */
+public class DisappearedNums {
+    public static void main(String[] args) {
+        int range=5;
+        int arr[]= {2,4,2};
+        System.out.println(Arrays.toString(missingNums(arr,range)));
+    }
+
+    //approach:
+
+}
