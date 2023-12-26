6ad425d8493f17f64871f8da8b0a2b814983b92f
SDE47 <sanketdevghare@gmail.com>
2023-12-26 20:34:16 +0530

    Updated the new examples.

---
+++ Recursion/Fibnaccii.java
@@ -0,0 +1,21 @@
+package Sanket.Recursion;
+//Get the nth fibonanci numbder.
+public class Fibnaccii {
+    public static void main(String[] args) {
+        int n=4;
+        System.out.println(fib(n));
+    }
+    static int fib(int n)
+    {
+        //Base condition
+        if(n<2) {
+            return n;
+        }
+        //Body
+
+        return fib(n-1)+ fib(n-2);
+
+
+        //Recursive call.
+    }
+}
